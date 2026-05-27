ed0d17fe10dc440e5091b2de9e5091a31d55e005
Yekchey Flores <yekcheyf@gmail.com>
2026-05-27 15:42:41 +0800

    try stash

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,7 +33,7 @@
                 <a href="{{ route ('employee.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-users"></i>
                     <p>
-                        {{ __('Employee Management') }}
+                        {{ __('Employee/manager/student/miko/ Management') }}
                     </p>
                 </a>
             </li>
